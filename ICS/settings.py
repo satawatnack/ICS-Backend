78a56c61e2f9775ef2b8f623720a7681b3bf84fd
Poowis <60070184@kmitl.ac.th>
2019-04-28 21:19:40 +0700

    save

--- ICS/settings.py
+++ ICS/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +63,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            ],
+            'builtins': [
+                'django.contrib.staticfiles.templatetags.staticfiles',
             ],
         },
     },
@@ -118,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "main/static"),
+]
---
+++ main/static/main/js/main.js
@@ -0,0 +1,15 @@
+// let navbar = new Vue({
+//     el: '#navbar',
+//     mounted: function () {
+//         window.addEventListener("scroll", function () {
+//             navbar.top = document.body.scrollTop;
+//         });
+//     },
+//     data: {
+//         top: 0,
+//     },
+//     computed: {
+//     },
+//     methods: {
+//     }
+// });
